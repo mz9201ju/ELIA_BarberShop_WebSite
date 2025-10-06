0594bbcfdfe4aa42be8d06c09b381138a850c869
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2025-10-05 19:34:13 -0700

    removed the / from end

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
 plugins: [react()],
-base: '/ELIA_BarberShop_WebSite/', // 👈 match your repo name exactly (case-sensitive)
+base: '/ELIA_BarberShop_WebSite', // 👈 match your repo name exactly (case-sensitive)
 })
