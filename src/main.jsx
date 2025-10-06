c016be6233e0504ad14980d9e6147f5b044fb95f
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2025-10-05 20:01:01 -0700

    added missing dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/ELIA_BarberShop_WebSite">
+  <BrowserRouter basename="/ELIA_BarberShop_WebSite/">
     <App />
   </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-plugins: [react()],
-base: '/ELIA_BarberShop_WebSite', // 👈 match your repo name exactly (case-sensitive)
-})
+  base: "/ELIA_BarberShop_WebSite/",  // <-- exact repo name, exact casing
+  plugins: [react()],
+});
